c9950fd2b49de46f9d3b74c4edb10fc53626d496
username <wecp-dummy@gmail.com>
2024-08-04 14:58:49 +0000

    Day 18 to 21

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+// import { Account } from '../../types/Account';
+import { AccountTS } from '../../types/tstypes/Accountts';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
-export class AccountComponent {
- 
+@Component({
+    selector: 'app-accounts',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+    onSubmit() {
+        throw new Error('Method not implemented.');
+    }
+    accountForm!: FormGroup;
+    account: AccountTS | undefined;
 
+
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        // No need to fetch data from a service since we are using hardcoded data
+        this.accountForm = this.fb.group({
+            account_id: [null, [Validators.required, this.validateAccountId]],
+            customer_id: [null, Validators.required],
+            balance: [null, [Validators.required, this.validateNonNegativeAmount]],
+        });
+        this.account = new AccountTS("1", 1000.00, "1");
+
+    }
+    validateAccountId(control: FormControl) {
+        const accountId = control.value;
+        return accountId === null ? { accountIdRequired: true } : null;
+    }
+
+    validateNonNegativeAmount(control: FormControl) {
+        const amount = control.value;
+        return amount < 0 ? { nonNegativeAmount: true } : null;
+    }
+
+    validateCustomerName(control: FormControl) {
+        const customerName = control.value;
+        const specialCharacterPattern = /[!@#$%^&*(),.?":{}|<>]/;
+        return specialCharacterPattern.test(customerName) ? { containsSpecialCharacters: true } : null;
+    }
 }
--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -5,7 +5,6 @@ import { CustomerTS } from '../../types/tstypes/Customerts';
 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
   imports: [],
   templateUrl: './customerarray.component.html',
   styleUrls: ['./customerarray.component.css']
